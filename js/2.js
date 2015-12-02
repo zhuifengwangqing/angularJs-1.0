df6fd40cb81c1cef431edebfec4fb6a96fa39342
jack <1432390933@qq.com>
2015-12-02 18:15:01 +0800

    用户表单展现

--- js/2.js
+++ js/2.js
@@ -1,3 +1,11 @@
 /**
  * Created by Administrator on 2015/12/2.
  */
+var myapp=angular.module('myapp',[]);
+myapp.controller('userCtrl', function ($scope,$http) {
+    $http_post=$http.post("http://angular_1.0_server.com/index.php/Home/Index");
+    $http_post.success(function(response){
+        $scope.users=response.list;
+    });
+
+});
