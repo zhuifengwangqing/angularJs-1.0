14803b5afc8772a3217c94b32d5d302f964bdce8
jack <1432390933@qq.com>
2015-12-04 18:07:19 +0800

    2

--- js/2.js
+++ js/2.js
@@ -3,30 +3,81 @@
  */
 var myapp=angular.module('myapp',[]);
 myapp.controller('userCtrl', function ($scope,$http) {
+    //初始化字段
+    $scope.user_name="";
+    $scope.user_sex="";
+    $scope.user_phone="";
+    $scope.user_id="";
+    $scope.edit=true;
+    $scope.incomplete=false;
+
     $http_post=$http.post("http://angular_1.0_server.com/index.php/Home/Index");
     $http_post.success(function(response){
         $scope.users=response.list;
     });
 
-    //初始化字段状态
-    $scope.user.name="";
-    $scope.user.phone="";
-    $scope.edit=true;
-    $scope.error=false;
-    $scope.incomplete=false;
+    //编辑信息
+    $scope.editUser=function(id){
+        $scope.edit=false;
+        $scope.incomplete=false;
+        $scope.user_name=$scope.users[id-1].f_name;
+        var sex=$scope.users[id-1].f_sex;
+        $scope.user_sex=(sex=="男")?1:0;
+        $scope.user_phone=$scope.users[id-1].f_phone;
+        $scope.user_id=$scope.users[id-1].f_id;
+    };
+
+    //添加信息
+    $scope.addUser=function(){
+        $scope.edit=true;
+        $scope.user_name="";
+        $scope.user_sex="";
+        $scope.user_phone="";
+        $scope.user_id="";
+    };
+
+    $scope.submit1=function(){
+        console.log($scope.user_id);
+        console.log($scope.user_name);
+        var formdata={
+            'f_id':$scope.user_id,
+            'f_name':$scope.user_name,
+            'f_sex':$scope.user_sex,
+            'f_phone':$scope.user_phone
+        };
+        //向数据库修改或提交数据
+        $http_post1=$http.post(
+            "http://angular_1.0_server.com/index.php/Home/moddata",
+            formdata
+        );
+
+        $http_post.success(function(response){
+            $scope.users=response.list;
+        });
+
+    };
 
 
-    //
-    //$scope.addUser=function(id){
-    //    //添加用户
-    //    if(id=="new"){
-    //        $scope.edit=true;
-    //        $scope.incomplete=true;
-    //
-    //
-    //
-    //    }
-    //};
+    //字段检测
+    $scope.$watch("user_phone",function(){
+        $scope.test();
+    });
+
+    $scope.test=function(){
+        var test=/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+
+        if(!$scope.user_name.trim().length||!$scope.user_sex.length){
+             $scope.incomplete=true;
+        }else{
+             $scope.incomplete=false;
+        }
+
+        if(!(test).test($scope.user_phone.trim())){
+            $scope.incomplete=true;
+        }else{
+            $scope.incomplete=false;
+        }
+    };
 
 
 });
