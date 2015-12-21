579bfcff6ad24f198d755d02dc22de38688dbed3
jack <1432390933@qq.com>
2015-12-21 19:05:48 +0800

    45

--- js/2.js
+++ js/2.js
@@ -38,26 +38,46 @@ myapp.controller('userCtrl', function ($scope,$http) {
     };
 
     $scope.submit1=function(){
-        console.log($scope.user_id);
-        var formdata={
-            'f_id':$scope.user_id,
-            'f_name':$scope.user_name,
-            'f_sex':$scope.user_sex,
-            'f_phone':$scope.user_phone
-        };
 
-        console.log(JSON.stringify(formdata)+"===");
+        //var formdata={
+        //    'f_id':$scope.user_id,
+        //    'f_name':$scope.user_name,
+        //    'f_sex':$scope.user_sex,
+        //    'f_phone':$scope.user_phone
+        //};
+
+        //console.log(JSON.stringify(formdata)+"===");
         //向数据库修改或提交数据
-        $http_post1=$http.post(
-            "http://angular_1.0_server.com/index.php/Home/Index/modData",
-            JSON.stringify(formdata)
 
-        );
-
-        $http_post1.success(function(response){
-            $scope.users=response.list;
+        $http_get=$http.get("http://angular_1.0_server.com/index.php/Home/Index/modData?f_id="+$scope.user_id+"&f_name="+$scope.user_name+"&f_sex="+$scope.user_sex+"&f_phone="+$scope.user_phone+"");
+        $http_get.success(function (response){
+            if(response.error_code=="000000"){
+                alert("成功!");
+                $scope.users=response.list;
+                $scope.user_name="";
+                $scope.user_sex="";
+                $scope.user_phone="";
+                $scope.user_id="";
+                $scope.edit=false;
+            }else{
+                alert("失败!");
+            }
         });
+    };
 
+    //删除
+
+    $scope.delUser=function(id){
+        $http_get=$http.get("http://angular_1.0_server.com/index.php/Home/Index/deldata?id="+id+"");
+        $http_get.success(function(response){
+            if(response.error_code=="000000"){
+                alert("删除成功!");
+                $scope.users=response.list;
+            }else{
+                alert("删除失败!");
+            }
+
+        });
     };
 
 
